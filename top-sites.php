213541713d8885ee7fb635cf87d0c645653d7d0b
domkacore <dominykas.bobinas@gmail.com>
2025-03-27 17:37:46 +0200

    added top sites retrieving + basic pagination from json file, before ranking from api

--- top-sites.php
+++ top-sites.php
@@ -31,9 +31,7 @@ class Top_Sites_Plugin
     return self::$instance;
   }
 
-  // private function __construct()
-  // {
-  // }
+  private function __construct() {}
 
   private function setup_hooks()
   {
@@ -50,7 +48,6 @@ class Top_Sites_Plugin
     wp_enqueue_style('tsp-styles', TSP_PLUGIN_URL . 'assets/style.css', array(), '1.0.0');
     wp_enqueue_script('tsp-scripts', TSP_PLUGIN_URL . 'assets/script.js', array('jquery'), '1.0.0', true);
   }
-
 
   public function enqueue_admin_scripts($hook)
   {
@@ -91,6 +88,31 @@ class Top_Sites_Plugin
   public function render_main_page()
   {
     $apiKey = get_option('openpagerank_api_key');
+    $current_page = max(1, isset($_GET['paged']) ? intval($_GET['paged']) : 1);
+    $per_page = 100;
+
+    $json_path = TSP_PLUGIN_DIR . 'data/top-sites.json';
+
+    if (!file_exists($json_path)) {
+      echo '<div class="notice notice-error"><p>Data file not found.</p></div>';
+
+      return;
+    }
+
+    $json_data = file_get_contents($json_path);
+    $sites_data = json_decode($json_data, true);
+
+    if (empty($sites_data)) {
+      echo '<div class="notice notice-error"><p>No data available.</p></div>';
+
+      return;
+    }
+
+    $total_sites = count($sites_data);
+    $total_pages = ceil($total_sites / $per_page);
+    $offset = ($current_page - 1) * $per_page;
+
+    $sites_for_page = array_slice($sites_data, $offset, $per_page);
 ?>
     <div class="wrap">
       <h1>Top Sites</h1>
@@ -104,7 +126,39 @@ class Top_Sites_Plugin
           </p>
         </div>
       <?php endif; ?>
-      <p><?php esc_html_e('List of Top Sites will appear here.', 'top-sites-wp'); ?></p>
+
+      <table class="top-sites__table widefat fixed striped">
+        <thead>
+          <tr>
+            <th class="top_sites__table--rank">Rank</th>
+            <th>Domain</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($sites_for_page as $site) : ?>
+            <tr>
+              <td><?php echo esc_html($site['rank']); ?></td>
+              <td><?php echo esc_html($site['rootDomain']); ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+
+      <div class="tablenav bottom">
+        <div class="tablenav-pages">
+          <?php
+          echo paginate_links(array(
+            'base' => add_query_arg('paged', '%#%'),
+            'format' => '',
+            'prev_text' => __('&laquo;', 'top-sites-wp'),
+            'next_text' => __('&raquo;', 'top-sites-wp'),
+            'total' => $total_pages,
+            'current' => $current_page,
+            'type' => 'list'
+          ));
+          ?>
+        </div>
+      </div>
     </div>
   <?php
   }
